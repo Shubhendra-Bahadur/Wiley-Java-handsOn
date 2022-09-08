8e1fc7cf6c5759b1f2972c9b6af9b888315eabec
Sunny <Sunny@DESKTOP-LSAJU83>
2022-09-08 14:37:21 +0530

    Day3 changes: seperate class from main

--- Day3/src/ArrayOperation.java
+++ Day3/src/ArrayOperation.java
@@ -1,17 +1,4 @@
-import java.util.*;
-
-public class Intro1 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int numArr[] = new int[] { 1, 2, 3, 4, 5 };
-		ArrayOperation ao = new ArrayOperation();
-		Scanner s = new Scanner(System.in);
-		ao.performTask(numArr, numArr.length, s);
-
-	}
-
-}
+import java.util.Scanner;
 
 class ArrayOperation {
 
---
+++ Day3/src/Day3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Day3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numArr[] = new int[] { 1, 2, 3, 4, 5 };
+		ArrayOperation ao = new ArrayOperation();
+		Scanner s = new Scanner(System.in);
+		ao.performTask(numArr, numArr.length, s);
+
+	}
+
+}
