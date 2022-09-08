116faea093fe165490e60a1a84eb94f28743bc87
Sunny <Sunny@DESKTOP-LSAJU83>
2022-09-08 16:17:51 +0530

    Day3: Contructor practice

--- Day3/src/Day3.java
+++ Day3/src/Day3.java
@@ -4,10 +4,21 @@ public class Day3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int numArr[] = new int[] { 1, 2, 3, 4, 5 };
-		ArrayOperation ao = new ArrayOperation();
-		Scanner s = new Scanner(System.in);
-		ao.performTask(numArr, numArr.length, s);
+//		int numArr[] = new int[] { 1, 2, 3, 4, 5 };
+//		ArrayOperation ao = new ArrayOperation();
+//		Scanner s = new Scanner(System.in);
+//		ao.performTask(numArr, numArr.length, s);
+
+		Employee employee1 = new Employee();
+		
+		System.out.println(employee1.toString());
+		
+		employee1.empId = 5;
+		employee1.empName = "Shubhendra";
+		System.out.println(employee1.toString());
+
+		Employee employee2 = new Employee(2, "Ramendra");
+		System.out.println(employee2.toString());
 
 	}
 
