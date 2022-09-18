925a89e3544a1fd3fec889f543acc254d1706f98
Sunny <Sunny@DESKTOP-LSAJU83>
2022-09-18 17:10:47 +0530

    Day 3 Assifnment Ques 3

--- Day3_Assignment/Date.java
+++ Day3_Assignment/Date.java
@@ -1,5 +1,42 @@
-package Day3_Assignment;
-
 public class Date {
-	private int dd,mm,yy;
+	private int dd, mm, yy;
+	
+	Date(int dd, int mm, int yy){
+		this.dd = dd;
+		this.mm = mm;
+		this.yy = yy;
+	}
+	
+	public void displayDate() {
+		if(checkDate())
+			System.out.println("Date : "+dd+"/"+mm+"/"+yy);
+	}
+	
+	public boolean checkDate() {
+		if(yy<1 || yy>99){
+			System.out.println("Year should be between 1 and 99 (Only validate for 2000-2099)");
+			return false;
+		}
+		else if(mm<1 || mm>12) {
+			System.out.println("Month should be between 1 and 12");
+			return false;
+		}
+		else if((mm==1 || mm==3 || mm==5 || mm==7 || mm==8 || mm==10 || mm==12) && (dd<1 || dd>31)){
+			System.out.println("Date should be between 1 and 31");
+			return false;
+		}
+		else if((mm==4 || mm==6 || mm==9 || mm==11) && (dd<1 || dd>30)){
+			System.out.println("Date should be between 1 and 30");
+			return false;
+		}
+		else if((mm==2) && (yy%4==0) && (dd<1 || dd>29)){
+			System.out.println("Date should be between 1 and 29 (A Leap Year)");
+			return false;
+		}
+		else if((mm==2) && (yy%4!=0) && (dd<1 || dd>28)){
+			System.out.println("Date should be between 1 and 28 (Not a Leap Year)");
+			return false;
+		}
+		return true;
+	}
 }
---
+++ Day3_Assignment/DateUse.java
@@ -0,0 +1,38 @@
+
+public class DateUse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date d1 = new Date(-1,2,3);
+		d1.displayDate();
+		Date d2 = new Date(1,-2,3);
+		d2.displayDate();
+		Date d3 = new Date(1,2,-3);
+		d3.displayDate();
+		Date d4 = new Date(1,2,100);
+		d4.displayDate();
+		Date d5 = new Date(1,2,0);
+		d5.displayDate();
+		Date d6 = new Date(1,22,20);
+		d6.displayDate();
+		Date d7 = new Date(1,2,20);
+		d7.displayDate();
+		Date d8 = new Date(0,1,20);
+		d8.displayDate();
+		Date d9 = new Date(32,1,20);
+		d9.displayDate();
+		Date d10 = new Date(31,1,20);
+		d10.displayDate();
+		Date d11 = new Date(30,2,20);
+		d11.displayDate();
+		Date d12 = new Date(31,9,20);
+		d12.displayDate();
+		Date d13 = new Date(30,9,22);
+		d13.displayDate();
+		Date d14 = new Date(29,2,22);
+		d14.displayDate();
+		Date d15 = new Date(29,2,20);
+		d15.displayDate();
+	}
+
+}
